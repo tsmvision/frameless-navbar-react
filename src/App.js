02bf930c6b5ae3fbfbdce2f738406c72daf8fda2
lukelee <lukelee01@gmail.com>
2021-01-28 21:44:32 -0500

    update App.css to control second and third level menu using react state rather than css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,84 @@
-import React from "react";
-import "./App.scss";
+import React, { useState, useEffect } from "react";
+// open/closing menu using css
+// import "./App-css.scss";
+// open/close menu using Javascript/React
+import "./App-js.scss";
+
+const MENU_LEVEL = {
+  FIRST: "first",
+  SECOND: "second",
+};
 
 const App = () => {
+  const [firstLevelMenu, setFirstLevelMenu] = useState({
+    id: "",
+    isOpen: false,
+  });
+
+  const [secondLevelMenu, setSecondLevelMenu] = useState({
+    id: "",
+    isOpen: false,
+  });
+
+  const flushState = (level) => {
+    switch (level) {
+      case MENU_LEVEL.FIRST:
+        setFirstLevelMenu({
+          id: "",
+          isOpen: false,
+        });
+        break;
+      case MENU_LEVEL.SECOND:
+        setSecondLevelMenu({
+          id: "",
+          isOpen: false,
+        });
+        break;
+      default:
+        break;
+    }
+  };
+
+  const openMenu = (e, level) => {
+    switch (level) {
+      case MENU_LEVEL.FIRST:
+        setFirstLevelMenu({
+          id: e.target.name,
+          isOpen: true,
+        });
+        break;
+      case MENU_LEVEL.SECOND:
+        setSecondLevelMenu({
+          id: e.target.name,
+          isOpen: true,
+        });
+        break;
+      default:
+        break;
+    }
+  };
+
+  const closeMenu = (level) => {
+    switch (level) {
+      case "first":
+        flushState("first");
+        flushState("second");
+        break;
+      case "second":
+        setSecondLevelMenu({
+          id: "",
+          isOpen: false,
+        });
+        break;
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    console.log(firstLevelMenu, secondLevelMenu);
+  });
+
   return (
     <div className="App">
       <ul class="top-level-menu">
@@ -11,36 +88,52 @@ const App = () => {
         <li>
           <a href="#">Services</a>
         </li>
-        <li>
-          <a href="#">Offices</a>
-          <ul class="second-level-menu">
-            <li>
-              <a href="#">Chicago</a>
-            </li>
-            <li>
-              <a href="#">Los Angeles</a>
-            </li>
-            <li>
-              <a href="#">New York</a>
-              <ul class="third-level-menu">
-                <li>
-                  <a href="#">Information</a>
-                </li>
-                <li>
-                  <a href="#">Book a Meeting</a>
-                </li>
-                <li>
-                  <a href="#">Testimonials</a>
-                </li>
-                <li>
-                  <a href="#">Jobs</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="#">Seattle</a>
-            </li>
-          </ul>
+        <li
+          name="offices"
+          onMouseEnter={(e) => openMenu(e, "first")}
+          onMouseLeave={() => closeMenu("first")}
+        >
+          <a href="#" name="offices">
+            Offices
+          </a>
+          {firstLevelMenu.id === "offices" && firstLevelMenu.isOpen && (
+            <ul class="second-level-menu" id="offices">
+              <li>
+                <a href="#">Chicago</a>
+              </li>
+              <li>
+                <a href="#">Los Angeles</a>
+              </li>
+              <li
+                name="newYork"
+                onMouseEnter={(e) => openMenu(e, "second")}
+                onMouseLeavel={() => closeMenu("second")}
+              >
+                <a href="#" name="newYork">
+                  New York
+                </a>
+                {secondLevelMenu.id === "newYork" && secondLevelMenu.isOpen && (
+                  <ul class="third-level-menu">
+                    <li>
+                      <a href="#">Information</a>
+                    </li>
+                    <li>
+                      <a href="#">Book a Meeting</a>
+                    </li>
+                    <li>
+                      <a href="#">Testimonials</a>
+                    </li>
+                    <li>
+                      <a href="#">Jobs</a>
+                    </li>
+                  </ul>
+                )}
+              </li>
+              <li>
+                <a href="#">Seattle</a>
+              </li>
+            </ul>
+          )}
         </li>
         <li>
           <a href="#">Contact</a>
