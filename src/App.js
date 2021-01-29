b6222133e0a238b6331e05bf7ad18f35dcb31e55
lukelee <lukelee01@gmail.com>
2021-01-28 20:43:17 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul class="top-level-menu">
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">Offices</a>
+          <ul class="second-level-menu">
+            <li>
+              <a href="#">Chicago</a>
+            </li>
+            <li>
+              <a href="#">Los Angeles</a>
+            </li>
+            <li>
+              <a href="#">New York</a>
+              <ul class="third-level-menu">
+                <li>
+                  <a href="#">Information</a>
+                </li>
+                <li>
+                  <a href="#">Book a Meeting</a>
+                </li>
+                <li>
+                  <a href="#">Testimonials</a>
+                </li>
+                <li>
+                  <a href="#">Jobs</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">Seattle</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
